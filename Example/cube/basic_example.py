2ea30a9ecdaa46e4fafa06f73e4e43f851e1dfdd
jbjolly <jb.jolly@yahoo.fr>
2019-06-11 00:49:20 +0200

    added started documentation

--- Example/cube/basic_example.py
+++ Example/cube/basic_example.py
@@ -9,3 +9,10 @@ stacked=LineStacker.line_image.stack(   coords,
                                         imagenames=imagenames,
                                         fEm=1900.548e9,
                                         stampsize=16)
+
+import matplotlib.pyplot as plt
+
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax.plot(stacked[0])
+fig.show()
